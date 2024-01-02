0b21bcbf7e954c24bfc6b75a09e47f2da78a36ce
firatNEAR <firat@near.org>
2024-01-02 23:01:50 +0100

    Initial commit

---
+++ bevy_invaders/src/movement.rs
@@ -0,0 +1,18 @@
+use bevy::prelude::*;
+pub struct MovementPlugin;
+
+#[derive(Component, Debug)]
+pub(crate) struct Velocity {
+    pub(crate) value: Vec3,
+}
+impl Plugin for MovementPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, update_position);
+    }
+}
+
+fn update_position(mut query: Query<(&Velocity, &mut Transform)>, time: Res<Time>) {
+    for (velocity, mut transform) in query.iter_mut() {
+        transform.translation += velocity.value * time.delta_seconds();
+    }
+}
